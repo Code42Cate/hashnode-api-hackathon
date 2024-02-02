081316a304901c05747a00b80c8af2775f2829bb
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-02-03 00:06:08 +0100

    ci: runtime?

--- apps/web/app/api/cover/route.tsx
+++ apps/web/app/api/cover/route.tsx
@@ -7,8 +7,6 @@ import { cookies } from "next/headers";
 const WIDTH = 2400;
 const HEIGHT = 1260;
 
-export const runtime = "edge";
-
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const host = url.searchParams.get("host");
