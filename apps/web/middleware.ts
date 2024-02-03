67e5b136a732343f78439e0cf372e78328fb2c71
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-02-03 01:29:23 +0100

    debug: deploy

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -7,6 +7,9 @@ import type { Database } from "database/types";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
+  console.log(process.env.NEXT_PUBLIC_SUPABASE_URL!)
+  console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+
   // Create a Supabase client configured to use cookies
   const supabase = createMiddlewareClient<Database>({ req, res }, {
     supabaseUrl:  process.env.NEXT_PUBLIC_SUPABASE_URL!,
