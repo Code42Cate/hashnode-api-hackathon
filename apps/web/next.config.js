d39ce7eadd5d420d1be6a2322b4162d5bba19b6a
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-02-03 04:57:32 +0100

    fix: next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
   images: {
-    domains: ["cdn.hashnode.com", "media.dev.to", "localhost", "hashnode-api-hackathon.onrender.com"],
+    domains: ["cdn.hashnode.com", "media.dev.to", "localhost", "hashnode-api-hackathon.onrender.com", "puegijxqyzuokuoeclrk.supabase.co"],
   },
   experimental: {
     serverActions: true,
